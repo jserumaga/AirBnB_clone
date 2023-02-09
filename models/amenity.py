772503509d5b697185f50cf01027955c1593f124
jserumaga <j.serumaga@gmail.com>
2023-02-09 04:02:55 -0800

    files

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+
+
+#!/usr/bin/python3
+
+"""Define Amenity option"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    "Represent an amenity"""
+
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,10 @@
+"""Define City Class"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Represent city"""
+
+    state_id = ""
+    name = ""
